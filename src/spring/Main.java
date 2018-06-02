f411ebf89e1baff683db906b2d4b882b9f57c61f
青梅煮茶 <942943001@qq.com>
2018-06-03 00:07:19 +0800

    20180603

--- src/spring/Main.java
+++ src/spring/Main.java
@@ -8,8 +8,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ApplicationContext sa=new ClassPathXmlApplicationContext("IOC.xml");
-		Person p1=(Person) sa.getBean("Person");
-		System.out.println(p1);
+		Person p2=(Person) sa.getBean("Person2");
+		System.out.println(p2);
 	}
 
 }
