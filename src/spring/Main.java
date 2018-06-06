71d90376223f9962551fa6d3c85e0bc765850f6b
青梅煮茶 <942943001@qq.com>
2018-06-07 00:15:51 +0800

    20180607

--- src/spring/Main.java
+++ src/spring/Main.java
@@ -1,5 +1,8 @@
 package spring;
 
+import java.text.DateFormat;
+import java.util.Date;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,8 +11,12 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ApplicationContext sa=new ClassPathXmlApplicationContext("IOC.xml");
-		Person p2=(Person) sa.getBean("Person");
-		System.out.println(p2);
+		//Person p2=(Person) sa.getBean("Person");
+		//System.out.println(p2);
+		DateFormat df=(DateFormat) sa.getBean("dateFormat");
+		Date sdf=(Date) sa.getBean("Date");
+		System.out.println(sdf);
+		//System.out.println(df);
 	}
 
 }
