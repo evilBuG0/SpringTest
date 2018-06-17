4decd7a335384f092c91342ac61ca757b10fa85c
青梅煮茶 <942943001@qq.com>
2018-06-17 17:10:25 +0800

    20180617

--- src/com/spring/beans/aop/ComputerAspect.java
+++ src/com/spring/beans/aop/ComputerAspect.java
@@ -1,6 +1,10 @@
 package com.spring.beans.aop;
 
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -9,12 +13,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class ComputerAspect {
 	//execution切点表达式
-	@Before("execution(public int com.spring.beans.aop.Computerimpl.add(int , int ) )")
-	public void a() {
-		System.out.println("调用前置处理");
+	//*;通配符，所有
+	//..;在方法的参数列表中使用，不限定类型，顺序，个数
+	@Before("execution(public int com.spring.beans.aop.Computerimpl.*(..) )")
+	public void a(JoinPoint Jp) {
+		//在通知方法的内部，如果想访问连接点的细节需要JoinPoint：接口类型
+		Object[] args=Jp.getArgs();
+		String methodname=Jp.getSignature().getName();
+		
+		System.out.println("调用"+methodname+"前置处理,参数为："+Arrays.asList(args));
 	}
-	@After("execution(public int com.spring.beans.aop.Computerimpl.add(int , int ) )")
-	public void b() {
-		System.out.println("调用后置处理");
+	@After("execution(public int com.spring.beans.aop.Computerimpl.*(..) )")
+	public void b(JoinPoint Jp) {
+		Object[] args=Jp.getArgs();
+		String methodname=Jp.getSignature().getName();
+		System.out.println("调用"+methodname+"后置处理,参数为："+Arrays.asList(args));
+	}
+	//如何获取方法执行之后的返回值
+	@AfterReturning(pointcut="execution(public int com.spring.beans.aop.Computerimpl.*(..) )",returning="result")
+	public void c(JoinPoint Jp,Object result) {
+		Object[] args=Jp.getArgs();
+		String methodname=Jp.getSignature().getName();
+		System.out.println("调用"+methodname+"返回通知处理,参数为："+Arrays.asList(args)+"正常返回结果为："+result);
 	}
 }
--- src/com/spring/beans/aop/Main.java
+++ src/com/spring/beans/aop/Main.java
@@ -1,18 +1,27 @@
 package com.spring.beans.aop;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
 public static  void main(String[] args) {
 	/*	
 	Computer com=new Computerimpl();
 		int a=com.add(5, 3);
 		System.out.println(a);
-	*/
+	
 	//创建一个计算器类
 	Computer computer =new Computerimpl();
 	ComputerProxy proxy=new ComputerProxy(computer);
 	computer=proxy.getComputerproxy();
 	int result=computer.add(1, 2);
-	System.out.println(result);
+	System.out.println(result);*/
+	
+	ApplicationContext ac=new ClassPathXmlApplicationContext("IOC_scan.xml");
+	Computer com=(Computer) ac.getBean("computerimpl");
+	System.out.println(com.div(1, 2));
+	System.out.println(com.add(1, 2));
+	
 }
 
 }
